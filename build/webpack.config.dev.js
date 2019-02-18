670e62718df4f71eb28847649cc006f59ba3d26e
dmitriy-rv <dimarikrv@gmail.com>
2019-02-19 01:16:05 +0300

    first commit. Add project template

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,55 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss?$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content">
+    <div class="left">
+      <search-component />
+      <list-component />
+    </div>
+    <div class="right">
+      <content-component />
+    </div>
+  </div>
+</template>
+<script>
+import SearchComponent from './components/SearchComponent.vue'
+import ListComponent from './components/ListComponent.vue'
+import ContentComponent from './components/ContentComponent.vue'
+
+export default {
+  components: {
+    'search-component': SearchComponent,
+    'list-component': ListComponent,
+    'content-component': ContentComponent
+  }
+}
+</script>
